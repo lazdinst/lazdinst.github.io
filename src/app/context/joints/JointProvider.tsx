0ec8588ceb08a749f0a79d3d767fe29b8280f99e
lazdinst <lazdinst@gmail.com>
2025-01-07 09:56:53 -0500

    migrated provider to new file

---
+++ src/app/context/joints/JointProvider.tsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import { URDFJoint } from "../../../definitions";
+import { JointProviderProps } from "./joints.types";
+import JointContext from "./JointContext";
+
+export const JointProvider: React.FC<JointProviderProps> = ({ children }) => {
+  const [joints, setJointsState] = useState<{ [key: string]: URDFJoint }>({});
+
+  const setJoints = (newJoints: { [key: string]: URDFJoint }) => {
+    setJointsState(newJoints);
+  };
+
+  const updateJoint = (name: string, value: number) => {
+    const joint = joints[name];
+    if (joint) {
+      joint.setJointValue(value);
+    }
+  };
+
+  return (
+    <JointContext.Provider value={{ joints, setJoints, updateJoint }}>
+      {children}
+    </JointContext.Provider>
+  );
+};
