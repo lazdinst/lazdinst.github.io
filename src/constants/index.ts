e240aa2d4ecb3d11a021a0ba2be228e77890210a
Talis Lazdins <lazdinst@gmail.com>
2025-01-12 13:05:05 -0500

    added constants to reference.

---
+++ src/constants/index.ts
@@ -0,0 +1,21 @@
+export const JOINT_NAME_MAP: { [key: string]: string } = {
+  joint_1: "J1",
+  joint_2: "J2",
+  joint_3: "J3",
+  joint_4: "J4",
+  joint_5: "J5",
+  joint_6: "J6",
+};
+
+export const ALLOWED_JOINTS = Object.keys(JOINT_NAME_MAP).reduce(
+  (acc, jointName) => {
+    acc[jointName] = true;
+    return acc;
+  },
+  {} as { [key: string]: boolean }
+);
+
+export const JOINT_STREAM_INTERVAL = 100;
+
+export const JOINT_CONTROL_INCREMENT = 0.1;
+export const JOINT_CONTROL_CLAMP = 3.14;
