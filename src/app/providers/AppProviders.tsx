f0318dbe6c4795772f45c457883bde3ec32dcc32
lazdinst <lazdinst@gmail.com>
2025-01-16 21:09:38 -0500

    updated providers and integrated into app

--- src/app/providers/AppProviders.tsx
+++ src/app/providers/AppProviders.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { AppProvidersProps, ProviderComponent } from "./providers.types";
-import { JointProvider } from "../context/joints";
+import { JointProvider, CartesianProvider } from "../context";
 
-const providers: ProviderComponent[] = [JointProvider];
+const providers: ProviderComponent[] = [JointProvider, CartesianProvider];
 
 const AppProviders: React.FC<AppProvidersProps> = ({ children }) => {
   return (
